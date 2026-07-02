87948b71c6870f89fac26ee2b22ef2582d413e04
52hbd2026-pixel <52hbd.2026@gmail.com>
2026-07-03 00:07:57 +0900

    Add files via upload

--- party.js
+++ party.js
@@ -11,3 +11,16 @@ envelopeImg.addEventListener('click', () => {
 closeBtn.addEventListener('click' , () => {
   modal2.classList.remove('show');
 });
+
+const rabbit = document.querySelector('.uss52'); 
+const comingSoonModal = document.getElementById('comingSoonModal');
+
+// 🐰 うさぎがタップされたら「制作中だよ！」を出す！
+rabbit.addEventListener('click', () => {
+  comingSoonModal.style.display = 'block';
+});
+
+// 💬 メッセージをクリックしたら閉じる！
+comingSoonModal.addEventListener('click', () => {
+  comingSoonModal.style.display = 'none';
+});
